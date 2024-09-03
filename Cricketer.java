97634d6c691cfa6a1c98f8058aae0e97b03ec79a
SunilYadav092 <sunilyadavy092@gmail.com>
2024-09-03 19:47:52 +0530

    New

---
+++ Cricketer.java
@@ -0,0 +1,22 @@
+class Cricketer{
+     String name = "Virat";
+     int age = 35;
+     String color = "Brown";
+     String role = "Batsman";
+     String nationality= "Indian";  
+
+     void cricketerDetails(){
+         System.out.println(name);
+         System.out.println(age);
+         System.out.println(color);
+         System.out.println(role);
+         System.out.println(nationality); 
+}//end of criketerDetails method 
+public static void main(String args[])
+  {
+    System.out.println("main method started");
+       Cricketer c = new Cricketer();
+       c.cricketerDetails();
+       System.out.println("main method ended");
+    }//end of main method
+}//end of Cricketer class
