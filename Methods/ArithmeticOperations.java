99ef7ce53dbe0852ed7561b0a2271425a8109e80
SunilYadav092 <sunilyadavy092@gmail.com>
2024-09-20 14:29:39 +0530

    New

---
+++ Methods/ArithmeticOperations.java
@@ -0,0 +1,32 @@
+public class ArithmeticOperations {
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+    public static int divide(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            System.out.println("Error: Division by zero!");
+            return 0; // returning 0 as an error value
+        }
+    }
+    public static int modulus(int a, int b) {
+        return a % b;
+    }
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        System.out.println("Addition of " + num1 + " and " + num2 + ": " + add(num1, num2));
+        System.out.println("Subtraction of " + num1 + " and " + num2 + ": " + subtract(num1, num2));
+        System.out.println("Multiplication of " + num1 + " and " + num2 + ": " + multiply(num1, num2));
+        System.out.println("Division of " + num1 + " and " + num2 + ": " + divide(num1, num2));
+        System.out.println("Modulus of " + num1 + " and " + num2 + ": " + modulus(num1, num2));
+    }
+}
---
+++ Methods/SimpleMethod.java
@@ -0,0 +1,8 @@
+public class SimpleMethod {
+    public static void greet() {
+        System.out.println("Hello, welcome to Java methods!");
+    }
+    public static void main(String[] args) {
+        greet();
+    }
+}
