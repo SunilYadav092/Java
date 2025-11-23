e62eea054de5679427eb9613158f7d5191e6dd3b
Sunil <sunilyadavy092@gmail.com>
2025-11-23 09:30:04 +0530

    New

--- Class_Objects/Hello.java
+++ Class_Objects/Hello.java
@@ -2,5 +2,7 @@
 class Hello{
     public static void main(String args[]){
         System.out.println("Hello World");
+        System.out.println("Welcome to Java Programming");
+        System.out.println("This is a simple Java program.");
  }
 }
